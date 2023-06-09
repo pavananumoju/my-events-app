a406050e676257c5bf12cb42a8c460d8db3d9346
Pavan Kumar Anumoju <pavananumoju@gmail.com>
2023-06-09 21:10:23 +0530

    Event Details Page using components

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,8 +1,30 @@
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
+import EventItem from "@/components/events/event-item";
+import { getEventById } from "@/dummy-data";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
 function EventDetailsPage() {
-  return (
-    <div>
-      <h1>Event Details Page</h1>
-    </div>
+  
+    const router = useRouter();
+    console.log(router.query.eventid);
+
+    const event = getEventById(router.query.eventid);
+
+    if(!event){
+        return <p>No event with Id {router.query.eventid} found</p>
+    }
+  
+    return (
+    <Fragment>
+        <EventSummary title={event.title}/>
+        <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title}/>
+        <EventContent>
+            <p>{event.description}</p>
+        </EventContent>
+    </Fragment>
   );
 }
 
